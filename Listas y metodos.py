abf3bf6bdb371ab87b4b86dfcadb2a263d3111be
Jrojas76 <j.rojas.qx@gmail.com>
2024-05-30 18:59:18 -0400

    Listas y metodos

---
+++ Listas y metodos.py
@@ -0,0 +1,15 @@
+mi_lista = [1, 2, 3, "hola", True]
+cantidad = len(mi_lista)
+print(cantidad)
+
+
+
+
+
+# class Persona:
+#     def __init__(self, nombre, edad):
+#         self.nombre = nombre
+#         self.edad = edad
+
+#     def saludar(self):
+#         print(f"Hola, me llamo {self.nombre} y tengo {self.edad} años.")
